948369b323c7579e25bf703535b91008fd97014d
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-08-12 15:29:55 +0000

    generador terminado sin refactorizar

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Irio", "Gaston"];
+  let action = ["da clases", "da cursos"];
+  let how = [
+    "bastante bien",
+    "bastante regular",
+    "muy mal",
+    "de manera aceptable"
+  ];
+  let where = [
+    "a alumnos en 4Geeks",
+    "a alumnos en una academia online",
+    "en una escuela",
+    "en el ordenador"
+  ];
+
+  function generateExcuse() {
+    let whoRandom = who[Math.floor(Math.random() * who.length)];
+    let actionRnadom = action[Math.floor(Math.random() * action.length)];
+    let howRandom = how[Math.floor(Math.random() * action.length)];
+    let whereRandom = where[Math.floor(Math.random() * where.length)];
+
+    return (
+      whoRandom + " " + actionRnadom + " " + howRandom + " " + whereRandom + "."
+    );
+  }
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
